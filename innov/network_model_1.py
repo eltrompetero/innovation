6bfbf187d8976c9c3952d81a2e8572fde017e462
Ernesto Ortega <ortega@csh.ac.at>
2023-07-25 15:39:44 +0200

    updates and missing file
    added missing file network_model_probabilistic_scipy_integrate.py
    correct threshold version of mft for networks

--- innov/network_model_1.py
+++ innov/network_model_1.py
@@ -9,7 +9,7 @@ from scipy.optimize import minimize, root
 from scipy.interpolate import interp1d
 from cmath import sqrt
 import warnings
-import networkx as nx
+#import networkx as nx
 
 from workspace.utils import save_pickle
 from .utils import *
@@ -52,14 +52,11 @@ class RKPropagator1D():
         
         """ Graph generator
         """
-        G = nx.circulant_graph(N, [1])
-        G.remove_edge(0, N-1)      
-        self.H = nx.DiGraph()
-        self.H.add_edges_from(G.edges())
+        self.Ady = np.eye(N, k=1)
         
         """ Initializing parametrers 
         """
-        self.N = len(self.H)  # Total size of the system    
+        self.N = N  # Total size of the system    
         self.τo = τo          
         self.rd = rd
         self.r = r
@@ -68,7 +65,7 @@ class RKPropagator1D():
         self.tmax = tmax
         self.Δt = Δt
         self.n0 = n0 
-        self.λ = λ
+        self.λ = 1
         self.t = 0
         self.count = 1
         self.switch = 1
@@ -87,7 +84,7 @@ class RKPropagator1D():
             sub_ix = list(range(*sub_ix))
         #else:
         #    raise NotImplementedError
-        self.n[sub_ix] = n0
+        self.n[sub_ix[0]] = n0
         
         
         """Init obsolescence method
@@ -96,18 +93,12 @@ class RKPropagator1D():
         
         """Init populated subgraph
         """
-        self.H_sub = self.H.subgraph(sub_ix)
-        self.In_H_sub = np.array([i for i in self.H_sub])
+        self.In_H_sub = sub_ix
         
         """Init obsolescence and Innovation fronts
         """
-        self.In_Obs_Front = []
-        self.In_Inn = []
-        for i in self.H_sub.nodes():
-            if list(self.H_sub.predecessors(i))==[]:
-                self.In_Obs_Front.append(i)                #Obs front
-            if list(self.H_sub.neighbors(i))==[]:
-                self.In_Inn.append(i)                      #Innov front
+        self.In_Obs_Front = [sub_ix[0]]
+        self.In_Inn = [sub_ix[1]]
                 
         """ Init distance to space of posible
         """
@@ -122,15 +113,6 @@ class RKPropagator1D():
         for i in self.In_Obs_Front:
             self.x_obs[i]= λ
         
-        """ Getting adyacency matrix
-        """
-        
-        A = nx.adjacency_matrix(self.H, weight=None)
-        A = A.toarray()
-        nodes_sub = np.array(list(self.H.nodes()))
-        perm = nodes_sub.argsort()
-        self.Ady =np.transpose(np.transpose(A[:, perm])[:, perm])
-        
         """ Computing inverse of offspring vector
         """
         inverse_sons = np.sum(self.Ady, axis=1)
@@ -139,12 +121,10 @@ class RKPropagator1D():
         
         """ Initialize Fix Matrix
         """
-        self.Fix_M = (-rd)*np.eye(self.N, self.N)             # Set death
-        
-        for i in self.In_H_sub:                               # Set replication
-            self.Fix_M[i]+=r*self.Ady[i]*self.inverse_sons
-            if i in self.In_Inn:
-                self.Fix_M[i]-=r*self.Ady[i]*self.inverse_sons
+        self.Fix_M = (-rd) * np.eye(self.N)            # Set death    
+        self.Fix_R = r * self.Ady
+        self.Fix_P = r*I*(self.Ady - np.eye(self.N))
+        self.Fix_O = τo*(self.Ady - np.eye(self.N))
             
                 
     def propagate(self, *args, **kwargs):          # propagate density function by some amount of time
@@ -166,21 +146,37 @@ class RKPropagator1D():
         In_Obs_Front = self.In_Obs_Front
         In_Inn = self.In_Inn
         x = self.x
-        
+        space_possible = self.sites[np.sum(Ady[In_Inn], axis=0)>0]
         """ Creat vector of Growth
         """
-        Fix_V = np.zeros(self.N)
-        if In_H_sub!=[]:
-            Fix_V[In_H_sub]= G_in/len(In_H_sub)
+        In_H_sp = np.concatenate((In_H_sub, space_possible))
+        #print(In_H_sp, space_possible, In_H_sub)
+        n_in = np.zeros(self.N)
+        n_in[In_H_sub] = self.n[In_H_sub] 
+        self.Fix_V = (n_in>0) * self.G_in/(len(In_H_sub)-1)
         
         """ Runge kutta order 4 update equations
         """
         
-        k1 = n @ Fix_M + Fix_V
-        k2 = (n+k1/2) @ Fix_M + Fix_V
-        k3 = (n+k2/2) @ Fix_M + Fix_V
-        k4 = (n+k3) @ Fix_M + Fix_V
-        k = (k1 + 2*k2 + 2*k3 + k4) * Δt/6
+        k1 = self.Δt*(-self.rd*self.n + n_in @ self.Fix_R + self.Fix_V)
+        
+        k11 = np.zeros(self.N)
+        k11[In_H_sp] = k1[In_H_sp] 
+        
+        k2 = self.Δt*(-self.rd*(self.n+k1/2) + (n_in +k11/2)@ self.Fix_R + self.Fix_V)
+        
+        
+        k22 = np.zeros(self.N)
+        k22[In_H_sp] = k1[In_H_sp]
+        
+        k3 = self.Δt*(-self.rd*(self.n+k2/2) + (n_in +k22/2)@ self.Fix_R + self.Fix_V)
+        
+        k33 = np.zeros(self.N)
+        k33[In_H_sp] = k1[In_H_sp] 
+        
+        k4 = self.Δt*(-self.rd*(self.n+k3) + (n_in +k33)@ self.Fix_R + self.Fix_V)
+        
+        k = (k1 + 2*k2 + 2*k3 + k4)/6
         
         """ Update densities and time
         """
@@ -272,26 +268,10 @@ class RKPropagator1D():
                 to_remove = In_Obs_Front[:]
                 """ Remove nodes from obsolescence front and the populated subgraph if not in Innovation front
                 """
-                for i in to_remove:
-                    if i not in In_Inn:
-                        In_Obs_Front.pop(In_Obs_Front.index(i))           #remove from Obsolescence front
-                        In_H_sub.pop(In_H_sub.index(i))                   #remove from populated subgraph
-                        """ Check if any of the parents is in obs front
-                        """
-                        for j in self.H.neighbors(i):                
-                            """ Check if any of the parents of node is still in obs front
-                            """
-                            obs_count=0
-                            for q in self.H.predecessors(j):
-                                if q!=i:
-                                    if q in In_Obs_Front:
-                                        obs_count+=1
-                            """ Add to Obs front if no parent in Obs front
-                            """
-                            if obs_count==0:
-                                In_Obs_Front.append(j)                     #add to Obsolescence front
-                        Fix_M[i]-= r*self.Ady[i]*self.inverse_sons         #remove replication
-        
+                space_obs = self.sites[np.sum(Ady[to_remove], axis=0)>0]
+                to_obs = np.setdiff1d(space_obs, to_remove)
+                In_Obs_Front = to_obs
+                In_H_sub = np.setdiff1d(In_H_sub, to_remove)
         else:
             In_H_sub = [i for i in In_H_sub]
             In_Obs_Front_1 = In_Obs_Front[:]
@@ -319,19 +299,14 @@ class RKPropagator1D():
         
         """ update vectors and Matrices
         """
-        self.Fix_M = Fix_M
         self.In_Obs_Front = In_Obs_Front
-        self.In_Inn = In_Inn
         self.In_H_sub = In_H_sub
-        self.x = x
-        self.x_obs = x_obs
         
     def update_H_sub_and_Innov(self):
         
         """ Read parameters, variavles and vectors
         """
         n = self.n
-        Fix_M = self.Fix_M
         G_in =self.G_in
         In_H_sub = self.In_H_sub
         Δt=self.Δt
@@ -347,39 +322,37 @@ class RKPropagator1D():
         x = self.x
         t = self.t
         
-        In_H_sub = [i for i in In_H_sub]
-        #In_H_sub = In_H_sub+In_Obs_Front
-        space_poss = []
         In_Inn_1 = In_Inn[:]
         for i in In_Inn_1:
+            #print(i, x)
             x[i]+= n[i]*r*I*Δt
             if x[i]>=λ:
                 """ Remove node from Inn
                 """
-                x.pop(i)
+               
                 In_Inn.pop(In_Inn.index(i))
                 
                 """ Remove all the parents from Innovation front
                 """
-                
-                for j in self.H.predecessors(i):
-                    if j in In_Inn_1:
-                        In_Inn.pop(In_Inn.index(j))
-                        x.pop(j)
+                #for j in self.H.predecessors(i):
+                #    if j in In_Inn_1:
+                #        In_Inn.pop(In_Inn.index(j))
+                #        x.pop(j)
                 
                 """ Add all the offspring to Innovation front if not already in 
                 """
-                for j in self.H.neighbors(i):
-                    if j not in In_Inn:
-                        x[j]=0
-                        In_Inn.append(j)
-                        In_H_sub.append(j)
-                        Fix_M[i][j]+=r/(self.inverse_sons[i])
-        
+                space_poss = self.sites[self.sites[Ady[i]>0]]
+                #print(space_poss, In_Inn, In_Inn + list(space_poss))
+                In_Inn += list(space_poss)
+                In_H_sub = list(In_H_sub)+ list(space_poss)
+                #x.fromkeys(In_Inn, 0)
+                for j in space_poss:
+                    x[j]= 0
+                x.pop(i)    
+                #print(x, In_Inn, type(In_Inn), type(list(space_poss)))
+                    
         """ update vectors and Matrices
         """
-        self.Fix_M = Fix_M
-        self.In_Obs_Front = In_Obs_Front
         self.In_Inn = In_Inn
         self.In_H_sub = In_H_sub
         self.x = x
---
+++ innov/network_model_probabilistic_scipy_integrate.py
@@ -0,0 +1,533 @@
+# ====================================================================================== #
+# Network innovation model numerical solutions probabilistic fronts.
+# 
+# Author : Ernesto Ortega, ortega@csh.ac.at
+# ====================================================================================== #
+from numba import njit, jit
+from numba.typed import List
+from scipy.optimize import minimize, root
+from scipy.interpolate import interp1d
+from cmath import sqrt
+import warnings
+import math
+import jax.numpy as jnp
+from jax import jit as jaxjit
+from scipy.special import factorial
+from scipy.special import gammaln, logsumexp
+import jax.lax as lax
+from functools import partial
+
+from workspace.utils import save_pickle
+from .utils import *
+
+from scipy.sparse import bsr_array, eye
+from scipy.integrate import solve_ivp
+import numpy as np
+from scipy.signal import medfilt
+# Suggestions:
+
+#@partial(jaxjit, static_argnums=(0,1,2,3))
+def compute_P_O_1(N, τo, t, init):
+        logP_O = np.zeros(N)
+        for i in range(N):
+            logP_O[i] = np.log(τo)*i - τo*t + np.log(t)*i - gammaln(i+1)
+
+        return np.exp(logP_O - logsumexp(logP_O))
+    
+def fn(t, y, N, G_in, rd, Fix_R, Fix_P, Fix_O, Fix_a):
+    n = y[0:N]
+    #n_k = medfilt(n, 11)
+    P_I = y[N:2*N]
+    P_O = y[2*N:3*N]
+    P_I_cum = P_I[::-1].cumsum()[::-1]
+    P_O_cum = P_O.cumsum()
+    P_L = P_I_cum * P_O_cum
+    Fix_V = G_in * (P_L/np.sum(P_L))
+    sites = np.arange(N)
+    if (P_I*sites).sum() > (P_O*sites).sum():
+        result = np.concatenate(( (-rd*n + (n * P_L) @ Fix_R + Fix_V), ((n * P_I) @ Fix_P), P_O @ Fix_O ))
+    else:
+        result = np.concatenate(( (-rd*n + (n * P_L) @ Fix_R + Fix_V), ((n * P_I) @ Fix_P), np.zeros(N) )) 
+    return result
+
+def infinit_growth(t, y, N, G_in, rd, Fix_R, Fix_P, Fix_O, Fix_a):
+    parar=False
+    P_I = y[N:2*N]
+    P_O = y[2*N:3*N]
+    if np.sum(y)>500000:
+        parar=True
+        print("infinite growth")
+    sites = np.arange(N)
+    if t > 30 and (P_I*sites).sum() < (P_O*sites).sum():
+        print("collapse")
+        parar = True
+    #if t > 30 and (P_I*sites).sum() < (P_O*sites).sum():
+        #print("collapse")
+        #parar = True
+    return 0 if parar else 1
+    
+# 1. object-oriented code
+class RKPropagator1D():
+    def __init__(self, N, sub_ix=None, n0=0, τo = 0.3, rd = 0.3, r = 0.4, I = 0.8, G_in= 4., tmax = 100, Δt = 0.1, λ = 1., method='density'):
+        """
+        Parameters
+        ----------
+        N : int
+            Size of whole graph.
+        sub_ix : list of indices or twople, None
+            If list of indices, then fill in those values with n0. If a twople,
+            fill entries between the two indices with n0.
+        n0 : float, 0.
+            Initial value of density.
+        τo: float
+            obsolecence decay
+        rd: float
+            death rate
+        r: float
+            expansion rate
+        I: float
+            innovation rate
+        G_in: float
+            growth rate
+        tmax: float
+            max computing time
+        Δt: float
+            time step for update
+        λ: float
+            density threshold
+        method: str
+            propagator method:
+                1- 'density'
+                2- 'frequency'
+        """
+        
+        """ Initializing parametrers 
+        """
+        self.N = N
+        self.τo = τo          
+        self.rd = rd
+        self.r = r
+        self.I= I
+        self.G_in= G_in
+        self.tmax = tmax
+        self.Δt = Δt
+        self.n0 = n0 
+        self.λ = λ
+        self.t = 0
+        self.count = 1
+        self.switch = 1
+        self.method = method
+        """ Creating sites and densities
+        """
+        sites = np.arange(N)
+        n = np.zeros(N)
+        Fix_V = np.zeros(N)
+        P_L = np.zeros(N)
+
+        """ Initializing densities
+        """
+        n[sub_ix[0]:sub_ix[1]]= n0
+        
+        self.init = sub_ix[-1]
+        
+        """Init obsolescence and Innovation fronts
+        """
+        self.P_I = np.asarray([0. if i!=sub_ix[1]-1 else 1. for i in range(self.N)])
+        self.P_O = np.asarray([0. if i!=sub_ix[0] else 1. for i in range(self.N)])       
+        self.sites = sites
+        self.n = n
+        self.Fix_V = Fix_V
+        self.P_L = P_L
+        """ Getting adjacency matrix
+        """
+        
+        self.Ady = np.eye(self.N, k=1)
+        
+        """ Computing inverse of offspring vector
+        """
+        inverse_sons = np.sum(self.Ady, axis=1)
+        inverse_sons[inverse_sons==0.] = 1.
+        self.inverse_sons = 1./inverse_sons
+        
+        """ Initialize Fix Matrix
+        """
+        self.Fix_M = (-rd) * np.eye(self.N)            # Set death    
+        self.Fix_R = r * self.Ady
+        self.Fix_P = r*I*(self.Ady - np.eye(self.N))
+        self.Fix_O = τo*(self.Ady - np.eye(self.N))
+        
+        a = np.eye(N, k=0)
+        for i in range(1, 3):
+            a += np.eye(N, k=i)
+            a += np.eye(N, k=-i)
+        b = a.sum(axis=1)
+        b = 1/b
+        self.Fix_a = a *b
+        
+    def automaton_evolve(self):
+        
+        n = self.n
+        
+        
+        
+        
+    def propagate(self):
+        
+        
+        """Propagate density function by some amount of time.
+        """
+        infinit_growth.terminal = True
+        nio = np.concatenate((self.n, self.P_I, self.P_O))
+        
+        sol = solve_ivp(fn, [0, self.tmax], nio, args=(self.N, self.G_in, self.rd, self.Fix_R, self.Fix_P, self.Fix_O, self.Fix_a), events=infinit_growth)
+        self.t = sol.t
+        self.n = sol.y[0:self.N]
+        self.P_I = sol.y[self.N:2*self.N]
+        self.P_O = sol.y[2*self.N:3*self.N]
+    
+    
+        
+    def propagate_jax(self):
+        """Propagate density function by some amount of time."""
+        self.n, self.P_I = jax_propagate(self.n, self.Fix_R, self.Fix_P, self.G_in,
+                                         self.Δt, self.r, self.τo, self.rd, self.I,
+                                         self.N, self.init, self.P_I, self.P_O)
+        self.t += self.Δt
+        self.P_O = compute_P_O_1(self.N, self.τo, self.t, self.init)
+    
+    
+    
+    def propagate_with_P_O_jax(self):
+        """Propagate density function by some amount of time."""
+        self.n, self.P_I, self.P_O = jax_propagate(self.n, self.Fix_R, self.Fix_P, self.Fix_O, self.G_in,
+                                         self.Δt, self.r, self.τo, self.rd, self.I,
+                                         self.N, self.init, self.P_I, self.P_O)
+        self.t += self.Δt
+        
+    def run_and_save(self, save_dt=1., tmax=None, **kwargs):
+        """Propagate over multiple time steps and save simulation steps.
+        
+        Parameters
+        ----------
+        save_dt : float, 1.
+        tmax : float
+            End simulation time.
+        **kwargs
+            To pass into self.propagate.
+            
+        Returns
+        -------
+        list
+            Density at each time step.
+        list
+            System length at each time step.
+        list
+            Time.
+        """
+        
+        n = [self.n.toarray()]
+        t = [0]
+        P_I = [self.P_I.toarray()]
+        P_O = [self.P_O.toarray()]
+        tmax = tmax if not tmax is None else self.tmax
+        
+        import time
+        start = time.time()
+        print(tmax)
+        while self.t < tmax:
+            if self.method == "frequency":
+                self.propagate(**kwargs)
+            else:
+                self.propagate_with_P_O(**kwargs)
+
+            #H_sub_in_time.append(self.In_H_sub[:])
+            #Obs_in_time.append(self.In_Obs_Front[:])
+            #Inn_in_time.append(self.In_Inn[:])
+            
+           # if np.isclose(self.t%save_dt, 0):
+            n.append(self.n.toarray())
+            P_I.append(self.P_I.toarray())
+            P_O.append(self.P_O.toarray())
+            t.append(self.t)
+            #print(self.t)
+            self.t+=self.Δt
+            start = time.time()
+        
+        return n, P_I, P_O, t
+
+
+
+    
+class UnitSimulatonNetworks():
+    def __init__(self, N, Ady, sub_ix= [0, 1], obs_ix = [0], inn_ix = [1], n0 = 10, τo = 0.3, rd = 0.3, r = 0.4, I = 0.8, G_in= 4., tmax = 100, dt = 0.1, method='ant'):
+        """
+            Parameters
+            ----------
+            N : int
+                Size of whole graph.
+            sub_ix : list of indices or twople, None
+                If list of indices, then fill in those values with n0. If a twople,
+                fill entries between the two indices with n0.
+            obs_ix: list
+                list of sites in the obsolescence front
+            inn_ix: list
+                list of sites in the innovation front
+            Ady: np.array()
+                Adyacency Matrix
+            n0 : int or list
+                If int Initial value of density in each site in sub_ix.
+                If list, initial densities in the system
+            τo: float
+                obsolecence decay
+            rd: float
+                death rate
+            r: float
+                expansion rate
+            I: float
+                innovation rate
+            G_in: float
+                growth rate
+            tmax: float
+                max computing time
+            dt: float
+                time step for update
+            method: str
+                innovation method:
+                    1- 'ant'
+                    2- 'explorer'
+        """
+
+        """ Initializing parametrers 
+        """
+
+        self.N = N
+        self.τo = τo          
+        self.rd = rd
+        self.r = r
+        self.I= I
+        self.G_in= G_in
+        self.tmax = tmax
+        self.dt = dt
+        self.n0 = n0 
+        self.t = 0
+        self.count = 1
+        self.switch = 1
+        self.method = method
+        """ Creating sites and densities
+        """
+        self.sites = np.arange(N)
+        n = np.zeros(N)
+
+        """ Initializing densities
+        """
+        n[sub_ix]= n0
+        
+        self.n = n
+        """Init obsolescence and Innovation fronts
+        """
+        self.obs_front = np.array(obs_ix)
+        self.inn_front = np.array(inn_ix)
+        self.in_sub_pop = np.array(sub_ix)
+        """Getting adjacency matrix
+        """
+
+        self.Ady = Ady
+
+        """ Computing inverse of offspring vector
+        """
+        inverse_sons = np.sum(self.Ady, axis=1)
+        inverse_sons[inverse_sons==0.] = 1.
+        self.inverse_sons = 1./inverse_sons
+
+    def move_innovation_ant(self):
+
+            n = self.n
+            obs_front = self.obs_front
+            inn_front = self.inn_front
+
+            to_innov = np.random.random(len(inn_front)) < self.r*self.I*self.dt*n[inn_front]
+            new = [np.random.choice(self.sites[self.Ady[i]>0]) for i in inn_front[to_innov]]
+            if new!=[]:
+                #print(self.inn_front, new)
+                self.inn_front = np.concatenate((self.inn_front, np.array(new)))
+                self.inn_front = np.unique(self.inn_front)
+                self.in_sub_pop = np.concatenate((self.in_sub_pop, np.array(new)))
+                self.in_sub_pop = np.unique(self.in_sub_pop)
+            self.inn_front = np.setdiff1d(self.inn_front, inn_front[to_innov])
+    def move_innovation_explorer(self):
+
+            n = self.n
+            obs_front = self.obs_front
+            inn_front = self.inn_front
+
+            to_innov = np.random.random(len(inn_front)) < self.r*self.I*self.dt*n[inn_front]
+            for i in inn_front[to_innov]:
+                space_posible = self.sites[self.Ady[i]>0]
+                space_posible = np.setdiff1d(space_posible, self.in_sub_pop)
+                new = np.random.choice(space_posible)
+                #print(new)
+                self.inn_front = np.concatenate((self.inn_front, np.array([new])))
+                self.inn_front = np.unique(self.inn_front)
+                self.in_sub_pop = np.concatenate((self.in_sub_pop, np.array([new])))
+                self.in_sub_pop = np.unique(self.in_sub_pop)
+                if len(space_posible)==1:
+                    self.inn_front = np.setdiff1d(self.inn_front, i)
+            
+    def automaton_evolve(self):
+
+            n = self.n
+            obs_front = self.obs_front
+            inn_front = self.inn_front
+
+            """
+            Update the innovation front
+            """
+            if self.method =='ant':
+                self.move_innovation_ant()
+            else:
+                self.move_innovation_explorer()
+
+            """
+            Replication 
+            """
+            to_replicate = np.random.poisson((self.r*self.inverse_sons* n * self.dt) @ self.Ady)
+            #print(to_replicate)
+            self.n[self.in_sub_pop] += to_replicate[self.in_sub_pop]
+
+            """
+            Death
+            """
+            to_died = np.random.poisson(self.rd * n *self.dt)
+            self.n -= to_died                            
+            self.n[self.n<0]=0
+
+            """
+            Growth
+            """
+            G_dt = np.random.poisson(self.G_in*self.dt)
+            #print(G_dt)
+            to_include = np.random.choice(self.in_sub_pop ,G_dt)
+            to_include = np.unique(to_include, return_counts=True)
+            self.n[to_include[0]]+= to_include[1]
+
+            """
+            Update obsolescence front 
+            """
+            obs_not_inn = np.setdiff1d(self.obs_front, self.inn_front)
+
+            to_obsolete = np.random.random(len(obs_not_inn))<self.τo*self.dt
+            #print(to_obsolete)
+            to_obsolete = obs_not_inn[to_obsolete]
+            #print(to_obsolete)
+            self.obs_front =np.setdiff1d(self.obs_front, to_obsolete)
+            self.in_sub_pop =np.setdiff1d(self.in_sub_pop, to_obsolete)
+
+            to_add_obs = self.sites[np.sum(self.Ady[to_obsolete], axis=0)>0]
+            to_add_obs = np.intersect1d(to_add_obs, self.in_sub_pop)
+            self.obs_front = np.concatenate((self.obs_front, to_add_obs))                            
+            self.obs_front = np.unique(self.obs_front)
+     
+    def run_and_save(self, tmax= None):
+        """Propagate over multiple time steps and save simulation steps.
+                Returns
+        -------
+        list
+            Density at each time step.
+        list
+            System length at each time step.
+        list
+            Time.
+        """
+        
+        n = [np.copy(self.n)]
+        n1= np.copy(self.n)
+        t = [0]
+        P_I = list(self.inn_front[:])
+        P_O = list(self.obs_front[:])
+        tmax = tmax if not tmax is None else self.tmax
+        
+        import time
+        start = time.time()
+        #print(tmax)
+        while self.t < tmax:
+            self.automaton_evolve()
+            #print(self.t, self.n[0:10], n1[0:10])
+            n.append(np.copy(self.n))
+            P_I.append(self.inn_front[:])
+            P_O.append(self.obs_front[:])
+            t.append(self.t)
+            #print(self.t)
+            self.t+=self.dt
+            start = time.time()
+        
+        return n, P_I, P_O, t
+    
+                                                                     
+       
+@jaxjit
+def jax_propagate(n, Fix_R, Fix_P, G_in, Δt, r, τo, rd, I, N, init, P_I, P_O):
+    """Propagate density function by some amount of time.
+    """
+    P_L = jnp.cumsum(P_O) * jnp.cumsum(P_I[::-1])[::-1]
+    
+    # Create growth vector
+    Fix_V = G_in * P_L/(jnp.nextafter(0, 1) + jnp.sum(P_L))
+
+    # Runge kutta order 4 update equations
+    k1 = Δt*(-rd*n + (n*P_L) @ Fix_R + Fix_V)
+    q1 = Δt*(((n*P_I)) @ Fix_P)
+    #q1 = (P_I) @ Fix_P
+    
+    k2 = Δt*(-rd*(n+k1/2) +((n+k1/2)*P_L) @ Fix_R + Fix_V)
+    q2 = Δt*(((n+k1/2)*(P_I+q1/2)) @ Fix_P)
+    #q2 = ((P_I+q1/2)) @ Fix_P
+    
+    k3 = Δt*(-rd*(n+k2/2) +((n+k2/2)*P_L) @ Fix_R + Fix_V)
+    q3 = Δt*(((n+k2/2)*(P_I+q2/2)) @ Fix_P)
+    #q3 = ((P_I+q2/2)) @ Fix_P
+    
+    k4 = Δt*(-rd*(n+k3) +((n+k3)*P_L) @ Fix_R + Fix_V)
+    q4 = Δt*(((n+k2/2)*(P_I+q3)) @ Fix_P)
+    #q4 = ((P_I+q3)) @ Fix_P
+    
+    k = (k1 + 2*k2 + 2*k3 + k4)/6
+    q = (q1 + 2*q2 + 2*q3 + q4)/6
+  
+    return n + k, P_I + q
+
+@jaxjit
+def jax_propagate_with_P_O(n, Fix_R, Fix_P, Fix_O, G_in, Δt, r, τo, rd, I, N, init, P_I, P_O):
+    """Propagate density function by some amount of time.
+    """
+    P_L = jnp.cumsum(P_O) * jnp.cumsum(P_I[::-1])[::-1]
+    
+    # Create growth vector
+    Fix_V = G_in * P_L/(jnp.nextafter(0, 1) + jnp.sum(P_L))
+
+    # Runge kutta order 4 update equations
+    k1 = Δt*(-rd*n + (n*P_L) @ Fix_R + Fix_V)
+    q1 = Δt*(((n*P_I)) @ Fix_P)
+    w1 = (P_O) @ Fix_O
+    
+    k2 = Δt*(-rd*(n+k1/2) +((n+k1/2)*P_L) @ Fix_R + Fix_V)
+    q2 = Δt*(((n+k1/2)*(P_I+q1/2)) @ Fix_P)
+    w2 = ((P_O+w1/2)) @ Fix_O
+    
+    k3 = Δt*(-rd*(n+k2/2) +((n+k2/2)*P_L) @ Fix_R + Fix_V)
+    q3 = Δt*(((n+k2/2)*(P_I+q2/2)) @ Fix_P)
+    w3 = ((P_O+w2/2)) @ Fix_O
+    
+    k4 = Δt*(-rd*(n+k3) +((n+k3)*P_L) @ Fix_R + Fix_V)
+    q4 = Δt*(((n+k2/2)*(P_I+q3)) @ Fix_P)
+    w4 = ((P_O+w3)) @ Fix_O
+    
+    k = (k1 + 2*k2 + 2*k3 + k4)/6
+    q = (q1 + 2*q2 + 2*q3 + q4)/6
+    w = (w1 + 2*w2 + 2*w3 + w4)/6
+  
+  
+    return n + k, P_I + q, P_O + w
+
+
+import random
+
