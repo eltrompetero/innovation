9bae44e4432b424d5284788087db4fbc5bf7879d
Eddie Lee <edlee@csh.ac.at>
2024-11-18 09:52:15 +0100

    aligned plot.density_snapshot() with changes

--- innov/plot.py
+++ innov/plot.py
@@ -5,7 +5,7 @@ import numpy as np
 
 def density_snapshot(n, el, K, t, mean=False, **kwargs):
     """Plot density snapshots from automaton simulation. Show the first two
-    branches separately.
+    branches separately and either one replica over mean over replicas.
 
     Parameters
     ----------
@@ -17,19 +17,21 @@ def density_snapshot(n, el, K, t, mean=False, **kwargs):
     t : list-like
         List of time-indices to show.
     mean : bool, False
-        If True, mean over replicas.
+        If True, mean over replicas, else show only the first replica.
     **kwargs : dict
     """
     fig, ax = plt.subplots(figsize=(6,2))
 
     if mean: 
         for tix in t:
-            ax.plot(np.concatenate((n[tix,:,:el[0]].mean(0), n[tix,:,el[0]::K].mean(0))))
-            ax.plot(np.concatenate((n[tix,:,:el[0]].mean(0), n[tix,:,el[0]+1::K].mean(0))))
+            ax.plot(n[tix,:,::K].mean(0))
+            if K>1:
+                ax.plot(n[tix,:,1::K].mean(0))
     else:
         for tix in t:
-            ax.plot(np.concatenate((n[tix,0,:el[0]], n[tix,0,el[0]::K])))
-            ax.plot(np.concatenate((n[tix,0,:el[0]], n[tix,0,el[0]+1::K])))
+            ax.plot(n[tix,0,::K])
+            if K>1:
+                ax.plot(n[tix,0,1::K])
 
     ax.set(**kwargs)
     return fig
