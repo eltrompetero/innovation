09ea4ba864159daea11efa5977fbb1484a3ffeb6
Eddie Lee <edlee@csh.ac.at>
2025-01-02 11:04:59 +0100

    added plot.low_density_region()

--- innov/plot.py
+++ innov/plot.py
@@ -3,6 +3,7 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.interpolate import CubicSpline
 
 
 def density_snapshot(n, el, K, t, mean=False, replica_ix=0, **kwargs):
@@ -42,3 +43,36 @@ def density_snapshot(n, el, K, t, mean=False, replica_ix=0, **kwargs):
 
     ax.set(**kwargs)
     return fig
+
+def low_density_region(vo_plot, r0, I, K, gamma):
+    """Return low density region where N<L, rd as a function of vo.
+    
+    This has been fixed to work specifically for the values that are plotted in paper. 
+    For other values, some fine-tuning in solving the boundary condition may be 
+    necessary.
+
+    Returns
+    -------
+    ndarray
+        Solved rd corresponding to input range.
+    """
+    vo_range = 2 - np.logspace(-3, .2, 100)[::-1]
+    critical_rd = np.zeros_like(vo_range)
+    err = np.zeros_like(vo_range)
+    model = CompartmentModel(r0, I=I, gamma=gamma, K=K)
+    
+    for i, vo in enumerate(vo_range):
+        def cost(logrd):
+            rd = np.exp(logrd)
+            if vo<1.8 and rd>4:
+                return 1e10
+            return np.abs(model.L(vo=vo, rd=rd)[0] - model.N(vo=vo, rd=rd)[0])**2
+        sol = minimize(cost, .75, method='powell', tol=1e-10)
+        critical_rd[i] = np.exp(sol['x'][0])
+        err[i] = sol['fun']
+
+    x, y = vo_range[err<1e-10], critical_rd[err<1e-10]
+    spline = CubicSpline(x, y, extrapolate=True)
+    y = spline(vo_plot)
+    y[y<1] = 1
+    return y
