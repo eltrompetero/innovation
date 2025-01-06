updates with innov front vel calculation fixes - not done
added pipeline function for automating front velocity test
updated simple_calculations.py
    .solve_inn_lambda()
    .I_tilde_coefficient()
    moved .solve_lambda()->.solve_obs_lambda()
    .collapse_rd()
    still need to fix up CompartmentModel
updated utils.py w/ poisson
updates to automaton.py
    removed old code for handling automaton output and running
    .inn_front_loc()
    .obs_front_loc()
imports fixed in plot.py

innov/automaton.py
@@ -13,78 +13,79 @@ import numpy as np
 # ================ #
 # Helper functions #
 # ================ #
-def body_fun(xi, mi):
-    # Use lax.cond to perform the conditional logic
-    return cond(mi,
-                lambda _: False,  # If mi is True, return value
-                lambda _: xi,     # If mi is False, return xi
-                operand=None)
-set_false = vmap(body_fun)
+def inn_front_loc(inn_front, samples, el, K, return_max=False, pinned=False):
+    """Returns mean location of innovation front by time step by replica.
+    
+    Parameters
+    ----------
+    inn_front : jnp.array
+    samples : int
+        Number of random replicas.
+    el : int
+        Total number of generations.
+    K : int
+        Number of branches.
+    return_max : bool, False
+        If True, return only the furthest location on each branch.
+    """
+    assert inn_front.ndim==3 and inn_front.shape[2]==el*K
 
-def body_fun(xi, mi):
-    # Use lax.cond to perform the conditional logic
-    return cond(mi,
-                lambda _: True,  # If mi is True, return value
-                lambda _: xi,     # If mi is False, return xi
-                operand=None)
-set_true = vmap(body_fun)
+    t = inn_front.shape[0]
+    mean_loc = np.zeros((t, samples))
+    x = inn_front.reshape(t, samples, el, K)
+    
+    if return_max:
+        counter = np.arange(el)  # useful var for tracking index
+        for t_ in range(t):
+            for i in range(samples):
+                m = []
+                for j in range(K):
+                    m.append(counter[x[t_,i,:,j]][-1])
+                if pinned:
+                    m = [max(m)-m_ for m_ in m]
+                mean_loc[t_, i] = np.mean(m)
+    else:
+        # mean of innov front locations along each branch per replica
+        counter = np.tile(np.arange(el), (K, 1)).T  # useful var for tracking index
+        for t_ in range(t):
+            for i in range(samples):
+                if pinned:
+                    mean_loc[t_, i] = (counter[x[t_,i,:,:]].max()-counter[x[t_,i,:,:]]).mean()
+                else:
+                    mean_loc[t_, i] = counter[x[t_,i,:,:]].mean()
+    return mean_loc
 
-def body_fun(xi, mi):
-    # Use lax.cond to perform the conditional logic
-    return cond(mi,
-                lambda _: 0,  # If mi is True, return value
-                lambda _: xi,     # If mi is False, return xi
-                operand=None)
-set_zero = vmap(body_fun)
-
-def compress_density(n):
-    """Compress density into a memory efficient representation.
+def obs_front_loc(obs_front, samples, el, K, pinned=False):
+    """Returns mean location of obsolescence front over all branches and then over replicas.
 
     Parameters
     ----------
-    n : jnp.ndarray
+    obs_front : jnp.array
+    samples : int
+        Number of random replicas.
+    el : int
+        Total number of generations.
+    K : int
+        Number of branches.
 
     Returns
     -------
-    jnp.ndarray
-        Density values.
-    jnp.ndarray
-        Corresponding indices.
+    ndarray
+        Avg location of obsolescence front per time point.
     """
-    ix = jnp.where(n)[0]
-    return n[ix], ix
-
-def decompress_density(n, ix, ix0=0, ix1=None):
-    """Decompress density from a memory efficient representation.
-
-    Parameters
-    ----------
-    n : jnp.ndarray
-        Density values.
-    ix : jnp.ndarray
-        Corresponding indices.
-    ix0 : int, 0
-        Starting index. If greater than the smallest value in ix, then the lower
-        value will be chosen as the new 0.
-    ix1 : int, None
-        Last index of array. Total array size shall be ix1-ix0+1.
-
-    Returns
-    -------
-    jnp.ndarray
-        Density array.
-    """
-    filled_n = jnp.zeros(ix.max()-min(ix.min(), ix0)+1, dtype=jnp.int32)
-    filled_n = filled_n.at[ix].set(n)
-
-    if ix1 is None:
-        return filled_n
-
-    if filled_n.size==ix1-ix0+1:
-        return filled_n
-    if filled_n.size<ix1-ix0+1:
-        return jnp.concatenate((filled_n, jnp.zeros(ix1-ix0+1-filled_n.size, dtype=jnp.int32)))
-    return filled_n[:ix1-ix0+1]
+    t = obs_front.shape[0]
+    mean_loc = np.zeros((t, samples))
+    x = obs_front.reshape(t, samples, el, K)
+    counter = np.arange(el)[:,None]  # helper var for tracking index
+    
+    for t_ in range(t):
+        for s in range(samples):
+            if pinned:
+                temp = [counter[ix].ravel()[-1] for ix in x[t_, s].T]
+                mean_loc[t_, s] = np.mean([max(temp)-t_ for t_ in temp])
+            else:
+                mean_loc[t_, s] = np.mean([counter[ix].ravel()[-1] for ix in x[t_, s].T])
+    return mean_loc
 
 def create_init_variables(el, K, n0):
     """Create a function to initialize variables for running the automaton.

innov/simple_calculations.py
@@ -4,6 +4,7 @@
 # Author : Eddie Lee, edlee@csh.ac.at
 # ====================================================================================== #
 from cmath import sqrt
+from functools import cache
 from .utils import *
 
 
@@ -32,7 +33,8 @@ def pde_pseudogap(y0, t, r0, I, r, rd, vo, gamma, K):
         Derivatives of variables.
     """
     N, L, n0, nl = y0
-    k = 1 + gamma * (K - 1) + gamma * K * solve_lambda(gamma)
+    vo *= vo_tilde_coefficient(gamma, K)
+    I *= I_tilde_coefficient(gamma, K)
 
     # assume system collapses fully if it falls below these limits
     if L<=2 or N<=0:
@@ -41,13 +43,22 @@ def pde_pseudogap(y0, t, r0, I, r, rd, vo, gamma, K):
         dn0 = -n0
         dnl = -nl
     else:
-        dN = r0 - rd*N + r*(N-n0) - vo*k*nl
-        dL = r*I*k*n0 - vo*k
-        dn0 = r0/L - rd*n0 + r*(N-n0-nl)/(L-2) - r*I*k*n0*n0
-        dnl = r0/L - rd*nl - vo*k*(nl - (N-n0-nl)/(L-2))
+        dN = r0 - rd*N + r*(N-n0) - vo*nl
+        dL = r*I*n0 - vo
+        dn0 = r0/L - rd*n0 + r*(N-n0-nl)/(L-2) - r*I*n0*n0
+        dnl = r0/L - rd*nl - vo*(nl - (N-n0-nl)/(L-2))
 
     return np.array([dN, dL, dn0, dnl])
 
+def collapse_rd(vo, r0, I, gamma, K):
+    """Define function for returning curve of collapse rd as a function of vo.
+    """
+    vo *= vo_tilde_coefficient(gamma, K)
+    I *= I_tilde_coefficient(gamma, K)
+    raise Exception("there is some sign error here. Check mathematica notebook carefully.")
+    return ((-4 * vo**3 + I * r0 * (1 + vo) + np.sqrt(16 * vo**4 + 8 * I * r0 * vo *
+            (1 + vo) + (I**2) * r0**2 * (1 + vo)**2)) / (4 * vo * (1 + vo)))
+    
 def critical_rd(r0, I, gamma, K):
     """Define function for returning curve of critical rd as a function of vo.
 
@@ -68,7 +79,7 @@ def critical_rd(r0, I, gamma, K):
         Takes vo as input and returns critical rd.
     """
     # Define function for root finding
-    lam = solve_lambda(gamma)
+    lam = solve_obs_lambda(gamma)
     k = 1 + gamma*(K-1) + gamma*K*lam
     naive_rd = lambda vo: (I*r0 + 3*vo - k*vo**2 - np.sqrt(((-I*r0 - 3*vo + k*vo**2)**2 -
                            8*vo*(-k*vo**2 - k**2*vo**3 + 2*np.sqrt(I*r0*vo + I*k**2*r0*vo**3)))))/(4*vo)
@@ -100,19 +111,15 @@ class CompartmentModel:
         vo = vo if vo is not None else self.vo
         gamma = gamma if gamma is not None else self.gamma
         K = K if K is not None else self.K
-        k = 1 + gamma*(K-1)
-        k1 = k + gamma*K*solve_lambda(gamma)
-        
-        A = I * K * (-1 + rd) * (rd + k1 * vo) * (-1 + 2 * rd**2 - k1**2 * vo**2 + rd * (-1 + k1 * vo))
-        B = I * K * r0 * (4 * rd**3 + rd**2 * (-2 + 5 * k1 * vo + k1**2 * vo**2) - 2 * (k1 * vo + k1**3 *
-            vo**3) + rd * (-2 - 2 * k1 * vo - k1**2 * vo**2 + k1**3 * vo**3)) + k1 * vo * (2 * k1 * vo + 5 *
-                    k1**3 * vo**3 + k1**5 * vo**5 - 2 * rd**3 * (2 + k1 * vo + k1**2 * vo**2) + rd * (2 + 2 *
-                        k1 * vo + 6 * k1**2 * vo**2 - 2 * k1**3 * vo**3) - rd**2 * (-2 + 3 * k1 * vo + 4 *
-                            k1**2 * vo**2 + 3 * k1**3 * vo**3))
-        C = (rd + k1 * vo)**2 * (I**2 * K**2 * r0**2 * rd**2 + k1**2 * vo**2 * (-2 * rd**2 + rd * (3 - k1 *
-            vo) + k1 * vo * (1 + k1 * vo))**2 + 2 * I * K * k1 * r0 * vo * (-2 * rd**3 + rd**2 * (3 - k1 *
-                vo) + k1 * rd * vo * (1 + k1 * vo) - 2 * (1 + k1**2 * vo**2)))
-        D = k1 * vo * (1 - k1 * vo)
+
+        # corrections
+        vo *= vo_tilde_coefficient(gamma, K) 
+        I *= I_tilde_coefficient(gamma, K)
+
+        A = I * (-1 + rd) * (rd + vo) * (-1 + 2 * rd**2 + rd * (-1 + vo) - vo**2)
+        B = I * r0 * (4 * rd**3 + rd**2 * (-2 + 5 * vo + vo**2) - 2 * (vo + vo**3) + rd * (-2 - 2 * vo - vo**2 + vo**3)) + vo * (2 * vo + 5 * vo**3 + vo**5 - 2 * rd**3 * (2 + vo + vo**2) + rd * (2 + 2 * vo + 6 * vo**2 - 2 * vo**3) - rd**2 * (-2 + 3 * vo + 4 * vo**2 + 3 * vo**3))
+        C = (rd + vo)**2 * (I**2 * r0**2 * rd**2 + vo**2 * (3 * rd - 2 * rd**2 + vo - rd * vo + vo**2)**2 + 2 * I * r0 * vo * (-2 * rd**3 - rd**2 * (-3 + vo) + rd * vo * (1 + vo) - 2 * (1 + vo**2)))
+        D = vo * (1 - vo) 
         quadform = (B + D * sqrt(C)) / (2 * A)
         return quadform, A, B, C
 
@@ -124,53 +131,17 @@ class CompartmentModel:
         vo = vo if vo is not None else self.vo
         gamma = gamma if gamma is not None else self.gamma
         K = K if K is not None else self.K
-        k = 1 + gamma*(K-1)
-        k1 = k + gamma*K*solve_lambda(gamma)
-        
-        A = k1 * (-1 + rd) * vo * (rd + k1 * vo)**2
-        B = (I * K * r0 * rd**2 + I * K * k1 * r0 * rd * vo - 3 * k1 * rd**2 * vo + 2 * k1 * rd**3 * vo - 4 *
-             k1**2 * rd * vo**2 + 3 * k1**2 * rd**2 * vo**2 - k1**3 * vo**3 - k1**4 * vo**4)
-        C = ((rd + k1 * vo)**2 * (4 * I * K * k1 * r0 * (-1 + rd) * vo * (1 + rd - 2 * rd**2 - k1 * rd * vo +
-            k1**2 * vo**2) + (I * K * r0 * rd - k1 * vo * (-2 * rd**2 + rd * (3 - k1 * vo) + k1 * vo * (1 +
-                k1 * vo)))**2))
+
+        # corrections
+        vo *= vo_tilde_coefficient(gamma, K) 
+        I *= I_tilde_coefficient(gamma, K)
+
+        A = (-1 + rd) * vo * (rd + vo)**2
+        B = -3 * rd**2 * vo + 2 * rd**3 * vo - 4 * rd * vo**2 + 3 * rd**2 * vo**2 - vo**3 - vo**4 + I * r0 * rd * (rd + vo)
+        C = (rd + vo)**2 * (I**2 * r0**2 * rd**2 + vo**2 * (3 * rd - 2 * rd**2 + vo - rd * vo + vo**2)**2 + 2 * I * r0 * vo * (-2 * rd**3 - rd**2 * (-3 + vo) + rd * vo * (1 + vo) - 2 * (1 + vo**2))) 
         quadform = (B + sqrt(C)) / (2 * A)
         return quadform, A, B, C
 
-    @classmethod
-    def N_as_fun(cls, r0, I, rd, vo, gamma, K):
-        """Steady state solution for N, total number of agents per branch for
-        compartment model."""
-        k = 1 + gamma*(K-1)
-        k1 = k + gamma*K*solve_lambda(gamma)
-        
-        A = I * K * (-1 + rd) * (rd + k1 * vo) * (-1 + 2 * rd**2 - k1**2 * vo**2 + rd * (-1 + k1 * vo))
-        B = I * K * r0 * (4 * rd**3 + rd**2 * (-2 + 5 * k1 * vo + k1**2 * vo**2) - 2 * (k1 * vo + k1**3 *
-            vo**3) + rd * (-2 - 2 * k1 * vo - k1**2 * vo**2 + k1**3 * vo**3)) + k1 * vo * (2 * k1 * vo + 5 *
-                    k1**3 * vo**3 + k1**5 * vo**5 - 2 * rd**3 * (2 + k1 * vo + k1**2 * vo**2) + rd * (2 + 2 *
-                        k1 * vo + 6 * k1**2 * vo**2 - 2 * k1**3 * vo**3) - rd**2 * (-2 + 3 * k1 * vo + 4 *
-                            k1**2 * vo**2 + 3 * k1**3 * vo**3))
-        C = (rd + k1 * vo)**2 * (I**2 * K**2 * r0**2 * rd**2 + k1**2 * vo**2 * (-2 * rd**2 + rd * (3 - k1 *
-            vo) + k1 * vo * (1 + k1 * vo))**2 + 2 * I * K * k1 * r0 * vo * (-2 * rd**3 + rd**2 * (3 - k1 *
-                vo) + k1 * rd * vo * (1 + k1 * vo) - 2 * (1 + k1**2 * vo**2)))
-        D = k1 * vo * (1 - k1 * vo)
-        quadform = (B + D * sqrt(C)) / (2 * A)
-        return quadform, A, B, C
-
-    @classmethod
-    def L_as_fun(cls, r0, I, rd, vo, gamma, K):
-        """Steady state solution for length of lattice along each branch for compartment model."""
-        k = 1 + gamma*(K-1)
-        k1 = k + gamma*K*solve_lambda(gamma)
-        
-        A = k1 * (-1 + rd) * vo * (rd + k1 * vo)**2
-        B = (I * K * r0 * rd**2 + I * K * k1 * r0 * rd * vo - 3 * k1 * rd**2 * vo + 2 * k1 * rd**3 * vo - 4 *
-             k1**2 * rd * vo**2 + 3 * k1**2 * rd**2 * vo**2 - k1**3 * vo**3 - k1**4 * vo**4)
-        C = ((rd + k1 * vo)**2 * (4 * I * K * k1 * r0 * (-1 + rd) * vo * (1 + rd - 2 * rd**2 - k1 * rd * vo +
-            k1**2 * vo**2) + (I * K * r0 * rd - k1 * vo * (-2 * rd**2 + rd * (3 - k1 * vo) + k1 * vo * (1 +
-                k1 * vo)))**2))
-        quadform = (B + sqrt(C)) / (2 * A)
-        return quadform, A, B, C
-    
     def gamma_runaway(self, r0=None, I=None, rd=None, vo=None, K=None):
         """Critical gamma delineating runaway boundary."""
         r0 = r0 if r0 is not None else self.r0
@@ -179,9 +150,19 @@ class CompartmentModel:
         vo = vo if vo is not None else self.vo
         K = K if K is not None else self.K
 
+        vo *= vo_tilde_coefficient(1, K)
+        I *= I_tilde_coefficient(1, K)
+
         def cost(loggamma):
             gamma = np.exp(loggamma)[0]
-            lam = solve_lambda(gamma)
+            if gamma>1: return 1e10
+            return np.abs(self.L(r0, I, rd, vo, gamma, K)[0])**2
+        return np.exp(minimize(cost, -1.)['x'])[0]
+
+        def cost(loggamma):
+            gamma = np.exp(loggamma)[0]
+            if gamma>1: return 1e10
+            lam = solve_obs_lambda(gamma)
             return ((-2 * K + (K * rd) / vo - 2 * K * lam + (K * rd * lam) / vo + (K * np.sqrt(-4
                     - 4 * rd + 9 * rd**2) * (1 + lam)) / vo) / (2 * (K**2 + 2 * K**2 * lam + K**2 *
                     lam**2)) - gamma)**2
@@ -198,13 +179,33 @@ class CompartmentModel:
         vo = vo if vo is not None else self.vo
         K = K if K is not None else self.K
 
+        vo *= vo_tilde_coefficient(1, K)
+        I *= I_tilde_coefficient(1, K)
+
         def cost(loggamma):
             gamma = np.exp(loggamma)[0]
+            if gamma>1: return 1e10
             return np.abs(self.L(r0, I, rd, vo, gamma, K)[0] - 2)**2
-        return np.exp(minimize(cost, 0.)['x'])[0]
+        return np.exp(minimize(cost, -1.)['x'])[0]
+
+@cache
+def solve_obs_lambda(gamma, g0=-1.):
+    """Solve for average distance of obsolescence front from leading one.
     
-def solve_lambda(gamma, g0=-1.):
+    Parameters
+    ----------
+    gamma : float
+        Connectivity.
+    g0 : float, -1.
+        Initial guess for optimization.
+    
+    Returns
+    -------
+    float
+        Average distance of obsolescence front from leading one.
+    """
     if hasattr(gamma, '__len__'):
+        assert np.all(0<=gamma) and np.all(gamma<=1)
         lam = np.zeros_like(gamma)
         for i, gamma_ in enumerate(gamma):
             def cost(loglam):
@@ -217,6 +218,7 @@ def solve_lambda(gamma, g0=-1.):
             lam[i] = np.exp(sol['x'])
         return lam
 
+    assert 0<=gamma<=1
     def cost(loglam):
         lam = np.exp(loglam)
         p0 = np.exp(-lam)
@@ -226,3 +228,87 @@ def solve_lambda(gamma, g0=-1.):
     sol = minimize(cost, g0)
     lam = np.exp(sol['x'])[0]
     return lam
+
+@cache
+def solve_inn_lambda(gamma, use_x1=False):
+    """Solve for average distance of innovation front from leading one.
+
+    Parameters
+    ----------
+    gamma : float
+        Connectivity.
+    use_x1 : bool, True
+        Whether to use x=1 condition as the leading innovation front.
+
+    Returns
+    -------
+    float
+        Average distance of innovation front from leading one.
+    """
+    assert 0<=gamma<=1
+
+    def cost(loglam):
+        lam = np.exp(loglam)
+        # this are inconsistent conditions, naturally b/c poisson is an assumption!
+        # but x=1 is slightly better when compared to samples
+        if use_x1:
+            term = (1-gamma) + lam**2/2 * (1-np.exp(-lam))*(1-np.exp(-lam)*lam)*gamma - lam
+        else:
+            term = gamma + gamma*lam*(1-np.exp(-lam)) - np.exp(-lam)
+        return term**2
+    
+    sol = minimize(cost, 0.)
+    lam = np.exp(sol['x'])[0]
+    return lam
+
+@cache
+def I_tilde_coefficient(gamma, K, mx_x=100):
+    """For solving for the correction to innovation front velocity.
+
+    Parameters
+    ----------
+    gamma : float
+        Connectivity.
+    K : int
+        Branching number.
+    mx_x : int, 100
+        Max (inclusive) value of x to which to calculate Poisson distribution.
+
+    Returns
+    -------
+    float
+        Correction factor to innovativeness. Multiply this to I to obtain Itilde in paper.
+    """
+    assert 0<=gamma<=1 and K>=1 and mx_x>=10
+
+    mx_x += 1
+    if gamma==0:
+        return 1.
+    if gamma==1:
+        return K
+        
+    lam = solve_inn_lambda(gamma)
+    p = poisson(np.arange(mx_x), lam)
+
+    term1 = 0.
+    term2 = 0.
+    for x in range(mx_x):
+        term1 += p[x]**2 * np.prod([1-p[xp] for xp in range(x)])
+    for x in range(mx_x):
+        term2 += p[x] * np.prod([1-p[xp] for xp in range(x)]) * sum([p[xpp] * (xpp-x) for xpp in range(x+1, mx_x)])
+
+    return term1 * (1 + gamma * (K-1)) + term2 * gamma * K
+
+@cache
+def vo_tilde_coefficient(gamma, K, mx_x=100):
+    """For solving for the correction to obsolescence front velocity.
+    """
+    assert 0<=gamma<=1 and mx_x>=10
+    mx_x += 1
+    i_range = np.arange(mx_x)
+
+    # correction to obsolescence
+    lam = solve_obs_lambda(gamma)
+    pk = poisson(i_range, lam)
+    correction = sum([pk[i]*(pk[:i+1]*(i-np.arange(i+1))).sum() for i in i_range]) * K * gamma
+    return (1+gamma*(K-1)) + correction